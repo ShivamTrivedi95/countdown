fe5a67c26dfc4e6e19f6ba4c2f85952e4cf14630
Shivam Trivedi <shivtrivedi197@gmail.com>
2023-12-20 20:38:32 +0530

    vite config changes

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/countdown",
   plugins: [react()],
   // other configurations
   build: {
     rollupOptions: {
-      input: "path/to/main.jsx",
+      input: "./src/main.jsx",
     },
   },
 });
